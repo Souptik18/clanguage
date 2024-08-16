0b45aac670ec837132876bf70a0d10ed9f886010
Souptik18 <souptiksarkar2017@gmail.com>
2024-08-16 14:38:48 +0530

    code

--- scanf.c
+++ scanf.c
@@ -7,3 +7,13 @@
 //   printf("%d", test);
 //   return 0;
 // }
+
+// #include <stdio.h>
+// int main()
+// {
+//   int test;
+//   char name[10];
+//   scanf("%d %s", &test, name);
+//   printf("%d %s", test, name);
+//   return 0;
+// }
